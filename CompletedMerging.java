b3d3c1e79273635a9c755e7b5142cb66cbdce435
cnidobyte <36839877+cnidobyte@users.noreply.github.com>
2022-11-09 14:22:09 -0500

    Add files via upload
    Implements a naive mergesort and shuffle algorithm

---
+++ CompletedMerging.java
@@ -0,0 +1,188 @@
+package edu.ser222.m02_02;
+import java.util.Random;
+
+/**
+ * Implements various divide and conquer algorithms.
+ *
+ * Last updated 4/2/2022.
+ *
+ * Completion time: (your completion time)
+ *
+ * @author (your name), Acuna, Sedgewick and Wayne
+ * @verison (version)
+ */
+import java.util.Random;
+
+public class CompletedMerging implements MergingAlgorithms {
+
+    //TODO: implement interface methods.
+    @Override
+    public <T extends Comparable> Queue<T> mergeQueues(Queue<T> q1, Queue<T> q2) {
+        //TODO: implement this!
+
+        Queue<T> mergedQueue = new ListQueue<>();
+        while(!(q1.isEmpty() && q2.isEmpty())) {
+            if (q1.isEmpty()) {
+                q2.dequeue();
+            } else if (q2.isEmpty()) {
+                q1.dequeue();
+            } else if (q1.peek().compareTo(q2.peek()) < 0) {
+                mergedQueue.enqueue(q1.dequeue());
+            } else mergedQueue.enqueue(q2.dequeue());
+        }
+        return mergedQueue;
+    }
+
+    @Override
+    public void sort(Comparable[] a) {
+        //TODO: implement this!
+        Comparable[] temp = mergesort(a);
+        for(int i = 0; i < a.length; i++) {
+            a[i] = temp[i];
+        }
+        assert isSorted(a);
+    }
+
+    @Override
+    public Comparable[] mergesort(Comparable[] a) {
+        //TODO: implement this!
+        int length = a.length;
+        int mid = length / 2 + length % 2;
+        if(length == 1) {return a;}      //base case
+
+        Comparable[] arr1 = new Comparable[mid];
+        for(int i = 0; i < arr1.length; i++) {
+            arr1[i] = a[i];
+        }
+
+        Comparable[] arr2 = new Comparable[length - mid];
+        for(int i = 0; i < arr2.length; i++) {
+            arr2[i] = a[mid + i];
+        }
+
+        arr1 = mergesort(arr1);
+        arr2 = mergesort(arr2);
+
+        return merge(arr1, arr2);
+    }
+
+    @Override
+    public Comparable[] merge(Comparable[] a, Comparable[] b) {
+        //TODO: implement this!
+        assert isSorted(a);
+        assert isSorted(b);
+        int i = 0, j = 0;
+        Comparable[] mergedArr = new Comparable[a.length + b.length];
+
+        for(int k = 0; k < mergedArr.length; k++) {
+            if(i > a.length - 1)       {mergedArr[k] = b[j++];}
+            else if(j > b.length - 1)  {mergedArr[k] = a[i++];}
+            else if(less(a[i], b[j]))  {mergedArr[k] = a[i++];}
+            else                       {mergedArr[k] = b[j++];}
+        }
+        return mergedArr;
+    }
+
+    @Override
+    public void shuffle(Object[] a) {
+        //TODO: implement this!
+        Object[] temp = mergeShuffle(a);
+        for(int i = 0; i < a.length; i++) {
+            a[i] = temp[i];
+        }
+    }
+
+    public Object[] mergeShuffle(Object[] a) {
+        Random randy = new Random();
+        int length = a.length;
+        int mid = length / 2 + length % 2;
+        if(length == 1) {return a;}
+
+        Object[] arr1 = new Comparable[mid];
+        for(int i = 0; i < arr1.length; i++) {
+            arr1[i] = a[i];
+        }
+
+        Object[] arr2 = new Comparable[length - mid];
+        for(int i = 0; i < arr2.length; i++) {
+            arr2[i] = a[mid + i];
+        }
+
+        arr1 = mergeShuffle(arr1);
+        arr2 = mergeShuffle(arr2);
+        return shuffler(arr1, arr2);
+    }
+
+    public Object[] shuffler(Object[] a, Object[] b) {
+        Random randy = new Random();
+        int i = 0, j = 0;
+        Object[] shuffledArray = new Object[a.length + b.length];
+
+        for(int k = 0; k < shuffledArray.length; k++) {
+            int index = randy.nextInt(2);
+            if(i > a.length - 1)       {shuffledArray[k] = b[j++];}
+            else if(j > b.length - 1)  {shuffledArray[k] = a[i++];}
+            else if(index == 0)        {shuffledArray[k] = a[i++];}
+            else                       {shuffledArray[k] = b[j++];}
+        }
+        return shuffledArray;
+    }
+     
+    /**
+     * entry point for sample output.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Queue<String> q1 = new ListQueue<>(); q1.enqueue("E"); q1.enqueue("L"); q1.enqueue("O"); q1.enqueue("R"); q1.enqueue("T");
+        Queue<String> q2 = new ListQueue<>(); q2.enqueue("A"); q2.enqueue("E"); q2.enqueue("M"); q2.enqueue("P"); q2.enqueue("S"); q2.enqueue("X");
+        Queue<Integer> q3 = new ListQueue<>(); q3.enqueue(5); q3.enqueue(12); q3.enqueue(15); q3.enqueue(17); q3.enqueue(20);
+        Queue<Integer> q4 = new ListQueue<>(); q4.enqueue(1); q4.enqueue(4); q4.enqueue(12); q4.enqueue(13); q4.enqueue(16); q4.enqueue(18);
+
+        MergingAlgorithms ma = new CompletedMerging();
+
+        //Q1 - sample test cases
+        Queue merged1 = ma.mergeQueues(q1, q2);
+        System.out.println(merged1.toString());
+        Queue merged2 = ma.mergeQueues(q3, q4);
+        System.out.println(merged2.toString());
+        
+        //Q2 - sample test cases
+        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        ma.sort(a);
+        assert isSorted(a);
+        show(a);
+        
+        //Q3 - sample test cases
+        String[] b = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        ma.shuffle(b);
+        show(b);
+        
+        ma.shuffle(b);
+        show(b);
+    }
+
+    //below are utilities functions, please do not change them.
+        
+    //sorting helper from text
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    //sorting helper from text
+    private static void show(Comparable[] a) {
+        for (Comparable a1 : a)
+            System.out.print(a1 + " ");
+
+        System.out.println();
+    }
+    
+    //sorting helper from text
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1]))
+                return false;
+        
+        return true;
+    }
+}
